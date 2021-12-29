3d741bf64237d5d1c3161344c61e5c0325979088
hanyanjiao <hanyanjiao@mininglamp.com>
2021-12-29 14:27:25 +0800

    add zk

---
+++ src/main/java/com/devilhan/zookeeper/Test.java
@@ -0,0 +1,94 @@
+package com.devilhan.zookeeper;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+
+/**
+ * @Author: devilHan
+ * @Date: 2021/12/29
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World");
+
+        /**
+         * zk是有session概念，没有连接池的概念
+         * zk watch 分为两类：
+         *  new zk时，传入的watch，这个watch，session级别的，与node,path无关
+         */
+
+        ZooKeeper zk = new ZooKeeper("10.211.55.3:2181,10.211.55.4:2181,10.211.55.5:2181,10.211.55.6:2181",
+                3000, new Watcher() {
+            @Override
+            public void process(WatchedEvent watchedEvent) {
+                Event.KeeperState state = watchedEvent.getState();
+                Event.EventType type = watchedEvent.getType();
+                String path = watchedEvent.getPath();
+
+                System.out.println(watchedEvent.toString());
+                switch (state) {
+                    case Unknown:
+                        break;
+                    case Disconnected:
+                        break;
+                    case NoSyncConnected:
+                        break;
+                    case SyncConnected:
+                        System.out.println("connect success");
+                        break;
+                    case AuthFailed:
+                        break;
+                    case ConnectedReadOnly:
+                        break;
+                    case SaslAuthenticated:
+                        break;
+                    case Expired:
+                        break;
+                    case Closed:
+                        break;
+                }
+
+                switch (type) {
+                    case None:
+                        break;
+                    case NodeCreated:
+                        break;
+                    case NodeDeleted:
+                        break;
+                    case NodeDataChanged:
+                        break;
+                    case NodeChildrenChanged:
+                        break;
+                    case DataWatchRemoved:
+                        break;
+                    case ChildWatchRemoved:
+                        break;
+                }
+            }
+        });
+
+        ZooKeeper.States state = zk.getState();
+        switch (state) {
+            case CONNECTING:
+                System.out.println("connecting");
+                break;
+            case ASSOCIATING:
+                break;
+            case CONNECTED:
+                System.out.println("connected");
+                break;
+            case CONNECTEDREADONLY:
+                break;
+            case CLOSED:
+                break;
+            case AUTH_FAILED:
+                break;
+            case NOT_CONNECTED:
+                break;
+        }
+    }
+}
